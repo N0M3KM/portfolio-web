177a062269c17b4c9fb6b01700f4156826134442
N0M3KM <trybrybetterthanu@gmail.com>
2024-10-15 17:08:34 +0700

    update function

--- libs/functions/CategorySelection.ts
+++ libs/functions/CategorySelection.ts
@@ -11,7 +11,7 @@ export const Selection = (title : string) => {
         case "database" : {
             return databaseLogo
         }
-        case "tool & library" : {
+        case "tool" : {
             return toolLogo
         }
     }
